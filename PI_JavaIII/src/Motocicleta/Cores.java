664f4516745f2c1373bc4caff3c057725a017352
IzabellaLima13 <izabellalimasales@gmail.com>
2015-04-23 20:40:09 -0300

    Projeto

---
+++ PI_JavaIII/src/Motocicleta/Cores.java
@@ -0,0 +1,6 @@
+package Motocicleta;
+
+public enum Cores {
+
+	PRETO, AMARELO, OURO, AZUL, PRATA, VERMELHO, BRANCO;
+}
---
+++ PI_JavaIII/src/Motocicleta/Montadora.java
@@ -0,0 +1,7 @@
+package Motocicleta;
+
+public enum Montadora {
+	
+	YAMAHA, HARLEY, SHADON,KASINSKI,HONDA, SUZUKI;
+
+}
